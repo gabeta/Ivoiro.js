6162a0c6c69f267a9a4aaf498cea7e9c63b961b0
Gabeta Soro <gabeta.soro@gmail.com>
2018-03-31 03:14:11 +0000

    Ajout de Mocha et Chai pour les test unitaires Javascript

---
+++ test/cfa-format.spec.ts
@@ -0,0 +1,12 @@
+import { expect } from 'chai';
+import 'mocha';
+
+describe('Hello function', () => {
+
+    it('should return hello world', () => {
+        const result = 1;
+        document.createElement('span');
+        expect(result).to.equal(1);
+    });
+
+});
